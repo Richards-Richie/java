38b650c6e9ce0847bef49ace18b37bcd3d8e4aeb
Richards-Richie <96327922+Richards-Richie@users.noreply.github.com>
2022-03-08 01:02:31 +0530

    Delete stringvalue.java

--- stringvalue.java
+++
@@ -1,6 +0,0 @@
-public class stringvalue{  
-    public static void main(String args[]){  
-    int value=30;  
-    String s1=String.valueOf(value);  
-    System.out.println(s1);//concatenating string with 10  
-    }}  
